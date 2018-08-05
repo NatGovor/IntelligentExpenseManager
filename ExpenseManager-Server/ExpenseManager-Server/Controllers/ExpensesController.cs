45f7a96f32ddadb4678977d9ae165e0715840a89
Natalia Govor <ngovor93@gmail.com>
2018-08-05 01:21:37 +0100

    Added basic server project structure.

---
+++ ExpenseManager-Server/ExpenseManager-Server/Controllers/ExpensesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseManager_Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseManager_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Expense>> GetAll()
+        {
+            return new List<Expense>
+            {
+                new Expense { Id = 0, Description = "Apartments", Amount = 100 },
+                new Expense { Id = 0, Description = "Train", Amount = 50 }
+            };
+        }
+    }
+}
