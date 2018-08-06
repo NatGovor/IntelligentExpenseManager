56de72ab24bb9a1c62008c5b63cb5aa83b6b2b1a
Natalia Govor <ngovor93@gmail.com>
2018-08-06 01:53:32 +0100

    Added skeleton for login component.

--- ExpenseManager.Client/src/app/unsecure-app/components/login/login.component.ts
+++ ExpenseManager.Client/src/app/unsecure-app/components/login/login.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'login',
     templateUrl: './login.component.html'
 })
 export class LoginComponent {
     email: string;
     password: string;
+
+    login(): void {
+        console.log("login");
+    }
 }
